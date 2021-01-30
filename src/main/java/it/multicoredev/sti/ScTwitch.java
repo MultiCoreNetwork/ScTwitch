4fbb9e25a2ac211bd260f56c09435e94a59aa127
Kevin Delugan <bisumto@gmail.com>
2021-01-30 11:11:38 +0100

    21w03a

--- src/main/java/it/multicoredev/sti/ScTwitch.java
+++ src/main/java/it/multicoredev/sti/ScTwitch.java
@@ -26,7 +26,7 @@ import it.multicoredev.sti.twitch.streamlabs.StreamlabsSocket;
 public class ScTwitch implements CarpetExtension {
     public static final String MOD_ID = "sctwitch";
     public static final String MOD_NAME = "ScTwitch";
-    public static final String MOD_VERSION = "1.1.0";
+    public static final String MOD_VERSION = "1.4.24";
 
     private Map<String, StreamlabsSocket> streamlabsSockets = new HashMap<>();
     private Map<String, TwitchChatSocket> twitchChatSockets = new HashMap<>();
